650b3c2f7219ae1dd7e942f528fa6d8f10823f0d
Fmesseder <fernando.messeder@gmail.com>
2020-06-03 07:19:58 -0400

    Refactor code to test for list row

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_later(self):
         #The user goes to check out the homepage of a new app
         self.browser.get('http://localhost:8000')
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         #lists "1: Buy peacock feathers" as an item in a todo list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         #There is still a text box inviting the user to add another item.
         #The user enters "Use the peacock feathers to make a fly"
@@ -46,11 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         #the page updates again and now show both items on the users list
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         #The user wonders whether the site will remember the list.
         #Then the user sees that the site has generated a unique URL for her
